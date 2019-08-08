6b5647842ab93f5510603522a875349eb4d1ea10
Vladimir Bauer <vbauerster@gmail.com>
2019-08-09 02:01:25 +0500

    add retfy feature

--- backoff.go
+++ backoff.go
@@ -1,144 +1,47 @@
 package backoff
 
 import (
-	"math/rand"
+	"context"
 	"time"
 )
 
-type BackoffStrategy interface {
-	Backoff(retries int) time.Duration
+// Func represents functions that can be retried.
+type Func func(attempt int, now time.Time) (retry bool, err error)
+
+// Strategy is a backoff methodology for retrying an operation.
+type Strategy interface {
+	NextAttempt(attempt int) time.Duration
 }
 
-type BackoffOption func(*BackoffConfig)
-
-type BackoffConfig struct {
-	state
-
-	// maxDelay is the upper bound of backoff delay.
-	maxDelay time.Duration
-
-	// baseDelay is the amount of time to wait before retrying after the first
-	// failure.
-	baseDelay time.Duration
-
-	// resetDelay iteration run duration, which if passed starts backoff from scratch.
-	resetDelay time.Duration
-
-	// factor is applied to the backoff after each retry.
-	factor float64
-
-	// jitter provides a range to randomize backoff delays.
-	jitter float64
-
-	rand *rand.Rand
-}
-
-type state struct {
-	count  int
-	reset  int
-	offdur time.Duration
-	iT     time.Time
-}
-
-// New initialises BackoffStrategy, which is NOT safe for concurrent use,
-// as it stores some state to keep track of when to reset.
-// For concurrent use, call New for each goroutine.
-func New(options ...BackoffOption) BackoffStrategy {
-	b := &BackoffConfig{
-		maxDelay:   180 * time.Second,
-		baseDelay:  1.0 * time.Second,
-		resetDelay: 1.0 * time.Hour,
-		factor:     1.6,
-		jitter:     0.2,
-		// not shared, not concurrent safe *rand.Rand instance
-		rand: rand.New(rand.NewSource(time.Now().UnixNano())),
+// Retry keeps trying the fn until it returns false, or no error is
+// returned. It will pause between retries, according to backoff
+// strategy.
+func Retry(ctx context.Context, strategy Strategy, reset time.Duration, fn Func) error {
+	if ctx == nil {
+		ctx = context.Background()
 	}
-	for _, option := range options {
-		option(b)
-	}
-	return b
-}
+	var err error
+	var cont bool
+	var attempt int
+	timer := time.NewTimer(0)
 
-func (bc *BackoffConfig) Backoff(retries int) (offdur time.Duration) {
-	defer func() {
-		bc.count++
-		bc.offdur = offdur
-		bc.iT = time.Now()
-	}()
-
-	if !bc.iT.IsZero() && time.Since(bc.iT) >= bc.resetDelay+bc.offdur {
-		bc.reset = retries - (retries - bc.count)
-	}
-
-	retries -= bc.reset
-	if retries == 0 {
-		return bc.baseDelay
-	}
-	backoff, max := float64(bc.baseDelay), float64(bc.maxDelay)
-	for backoff < max && retries > 0 {
-		backoff *= bc.factor
-		retries--
-	}
-	if backoff > max {
-		backoff = max
-	}
-	// Randomize backoff delays so that if a cluster of requests start at
-	// the same time, they won't operate in lockstep.
-	backoff *= 1 + bc.jitter*(bc.rand.Float64()*2-1)
-	if backoff < 0 {
-		return 0
-	}
-	return time.Duration(backoff)
-}
-
-// WithMaxDelay is the upper bound of backoff delay.
-// Default is 180 seconds.
-func WithMaxDelay(d time.Duration) BackoffOption {
-	return func(b *BackoffConfig) {
-		b.maxDelay = d
-	}
-}
-
-// WithBaseDelay is the amount of time to wait before retrying after the first
-// failure. Default is 1 second.
-func WithBaseDelay(d time.Duration) BackoffOption {
-	return func(b *BackoffConfig) {
-		b.baseDelay = d
-	}
-}
-
-// WithResetDelay is iteration run duration between retry to check,
-// which if passed starts backoff from scratch, i.e. from base delay.
-// Default is 1 hour.
-func WithResetDelay(d time.Duration) BackoffOption {
-	return func(b *BackoffConfig) {
-		b.resetDelay = d
-	}
-}
-
-// WithFactor is applied to the backoff after each retry.
-// Default value is 1.6
-func WithFactor(factor float64) BackoffOption {
-	return func(b *BackoffConfig) {
-		b.factor = factor
-	}
-}
-
-// WithJitter provides a range to randomize backoff delays.
-// Default value is 0.2
-func WithJitter(jitter float64) BackoffOption {
-	return func(b *BackoffConfig) {
-		b.jitter = jitter
-	}
-}
-
-// WithCustomRand override default *rand.Rand which is
-// seeded with rand.NewSource(time.Now().UnixNano()).
-func WithCustomRand(rand *rand.Rand) BackoffOption {
-	return func(b *BackoffConfig) {
-		if rand == nil {
-			return
+quit:
+	for {
+		select {
+		case now := <-timer.C:
+			cont, err = fn(attempt, now)
+			if !cont || err == nil {
+				break quit
+			}
+			if time.Since(now) >= reset {
+				attempt = 0
+			}
+			attempt++
+			timer.Reset(strategy.NextAttempt(attempt))
+		case <-ctx.Done():
+			timer.Stop()
+			return ctx.Err()
 		}
-		b.rand = rand
 	}
+	return err
 }
---
+++ exponential/exponential.go
@@ -0,0 +1,107 @@
+package exponential
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/vbauerster/backoff"
+)
+
+type Option func(*Exponential)
+
+type Exponential struct {
+	// maxDelay is the upper bound of backoff delay.
+	maxDelay time.Duration
+
+	// baseDelay is the amount of time to wait before retrying after the first
+	// failure.
+	baseDelay time.Duration
+
+	// factor is applied to the backoff after each retry.
+	factor float64
+
+	// jitter provides a range to randomize backoff delays.
+	jitter float64
+
+	rand *rand.Rand
+}
+
+// New initialises Strategy, which is NOT safe for concurrent use,
+// as it stores some state to keep track of when to reset.
+// For concurrent use, call New for each goroutine.
+func New(options ...Option) backoff.Strategy {
+	eb := Exponential{
+		maxDelay:  180 * time.Second,
+		baseDelay: 1.0 * time.Second,
+		factor:    1.6,
+		jitter:    0.2,
+		rand:      rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+	for _, option := range options {
+		option(&eb)
+	}
+	return eb
+}
+
+func (eb Exponential) NextAttempt(attempt int) time.Duration {
+	if attempt == 0 {
+		return eb.baseDelay
+	}
+	backoff, max := float64(eb.baseDelay), float64(eb.maxDelay)
+	for backoff < max && attempt > 0 {
+		backoff *= eb.factor
+		attempt--
+	}
+	if backoff > max {
+		backoff = max
+	}
+	// Randomize backoff delays so that if a cluster of requests start at
+	// the same time, they won't operate in lockstep.
+	backoff *= 1 + eb.jitter*(eb.rand.Float64()*2-1)
+	if backoff < 0 {
+		return 0
+	}
+	return time.Duration(backoff)
+}
+
+// WithMaxDelay is the upper bound of backoff delay.
+// Default is 180 seconds.
+func WithMaxDelay(d time.Duration) Option {
+	return func(eb *Exponential) {
+		eb.maxDelay = d
+	}
+}
+
+// failure. Default is 1 second.
+func WithBaseDelay(d time.Duration) Option {
+	return func(eb *Exponential) {
+		eb.baseDelay = d
+	}
+}
+
+// WithFactor is applied to the backoff after each retry.
+// Default value is 1.6
+func WithFactor(factor float64) Option {
+	return func(eb *Exponential) {
+		eb.factor = factor
+	}
+}
+
+// WithJitter provides a range to randomize backoff delays.
+// Default value is 0.2
+func WithJitter(jitter float64) Option {
+	return func(eb *Exponential) {
+		eb.jitter = jitter
+	}
+}
+
+// WithCustomRand override default *rand.Rand which is
+// seeded with rand.NewSource(time.Now().UnixNano()).
+func WithCustomRand(rand *rand.Rand) Option {
+	return func(eb *Exponential) {
+		if rand == nil {
+			return
+		}
+		eb.rand = rand
+	}
+}
