54dc59070716f8e936391431cec39f21783514a9
Vladimir Bauer <vbauerster@gmail.com>
2019-08-09 11:05:07 +0500

    doc comments

--- exponential/exponential.go
+++ exponential/exponential.go
@@ -7,9 +7,9 @@ import (
 	"github.com/vbauerster/backoff"
 )
 
-type Option func(*Exponential)
+type Option func(*strategy)
 
-type Exponential struct {
+type strategy struct {
 	// maxDelay is the upper bound of backoff delay.
 	maxDelay time.Duration
 
@@ -26,11 +26,11 @@ type Exponential struct {
 	rand *rand.Rand
 }
 
-// New initialises Strategy, which is NOT safe for concurrent use,
-// as it stores some state to keep track of when to reset.
-// For concurrent use, call New for each goroutine.
+// New initializes exponential backoff.Strategy.
+// It implements exponential backoff algorithm as defined in
+// https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 func New(options ...Option) backoff.Strategy {
-	eb := Exponential{
+	s := strategy{
 		maxDelay:  180 * time.Second,
 		baseDelay: 1.0 * time.Second,
 		factor:    1.6,
@@ -38,18 +38,18 @@ func New(options ...Option) backoff.Strategy {
 		rand:      rand.New(rand.NewSource(time.Now().UnixNano())),
 	}
 	for _, option := range options {
-		option(&eb)
+		option(&s)
 	}
-	return eb
+	return s
 }
 
-func (eb Exponential) Pause(attempt int) time.Duration {
+func (s strategy) Pause(attempt int) time.Duration {
 	if attempt == 0 {
-		return eb.baseDelay
+		return s.baseDelay
 	}
-	backoff, max := float64(eb.baseDelay), float64(eb.maxDelay)
+	backoff, max := float64(s.baseDelay), float64(s.maxDelay)
 	for backoff < max && attempt > 0 {
-		backoff *= eb.factor
+		backoff *= s.factor
 		attempt--
 	}
 	if backoff > max {
@@ -57,7 +57,7 @@ func (eb Exponential) Pause(attempt int) time.Duration {
 	}
 	// Randomize backoff delays so that if a cluster of requests start at
 	// the same time, they won't operate in lockstep.
-	backoff *= 1 + eb.jitter*(eb.rand.Float64()*2-1)
+	backoff *= 1 + s.jitter*(s.rand.Float64()*2-1)
 	if backoff < 0 {
 		return 0
 	}
@@ -67,41 +67,41 @@ func (eb Exponential) Pause(attempt int) time.Duration {
 // WithMaxDelay is the upper bound of backoff delay.
 // Default is 180 seconds.
 func WithMaxDelay(d time.Duration) Option {
-	return func(eb *Exponential) {
-		eb.maxDelay = d
+	return func(s *strategy) {
+		s.maxDelay = d
 	}
 }
 
 // failure. Default is 1 second.
 func WithBaseDelay(d time.Duration) Option {
-	return func(eb *Exponential) {
-		eb.baseDelay = d
+	return func(s *strategy) {
+		s.baseDelay = d
 	}
 }
 
 // WithFactor is applied to the backoff after each retry.
 // Default value is 1.6
 func WithFactor(factor float64) Option {
-	return func(eb *Exponential) {
-		eb.factor = factor
+	return func(s *strategy) {
+		s.factor = factor
 	}
 }
 
 // WithJitter provides a range to randomize backoff delays.
 // Default value is 0.2
 func WithJitter(jitter float64) Option {
-	return func(eb *Exponential) {
-		eb.jitter = jitter
+	return func(s *strategy) {
+		s.jitter = jitter
 	}
 }
 
 // WithCustomRand override default *rand.Rand which is
 // seeded with rand.NewSource(time.Now().UnixNano()).
 func WithCustomRand(rand *rand.Rand) Option {
-	return func(eb *Exponential) {
+	return func(s *strategy) {
 		if rand == nil {
 			return
 		}
-		eb.rand = rand
+		s.rand = rand
 	}
 }
